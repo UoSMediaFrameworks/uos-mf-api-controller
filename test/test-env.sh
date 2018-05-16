a783c824ddbb3ce09996cded50ebe897edd98a6f
Aaron Phillips <aaronp261@gmail.com>
2018-05-16 17:23:28 +0100

    Version bump.

--- test/test-env.sh
+++ test/test-env.sh
@@ -3,7 +3,7 @@
 # BEGIN EDITS
 export HUB_URL='http://dev-uos-mediahub.azurewebsites.net'
 export HUB_PASSWORD='kittens'
-export PORT=3000
+export PORT=3001
 export SWAGGER_JSON='/api-docs.json'
 export SWAGGER_HOST='localhost:3000'
 
